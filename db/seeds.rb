69d53e10d578f4a3311fe9abdcf4ded766a442a8
annedien <annedient@gmail.com>
2016-10-30 12:46:37 +0100

    carrierwave and deis setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Photo.delete_all
+Song.delete_all
+Artist.delete_all
+
+artist1 = Artist.create(name: "Rolling Stones")
+artist2 = Artist.create(name: "Jaques Brel")
+artist3 = Artist.create(name: "Prince")
+
+song1 = Song.create ( name: "I can get no satisfaction", artist: "Rolling Stones" )
+song2 = Song.create ( name: "Ne me quitte pas", artist:"Jaques Brel"   )
+song3 = Song.create  ( name: "Kiss", artist: "Prince")
+song4 = Song.create  ( name: "Purple rain", artist: "Prince" )
+
